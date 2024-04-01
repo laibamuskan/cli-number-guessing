b26e455af83b5fb967a6da09e2a333c2b0b14ce0
laibamuskan <164530270+laibamuskan@users.noreply.github.com>
2024-04-01 16:30:03 +0500

    push my cli-number-guessing

--- README.md
+++ README.md
@@ -1,1 +1,2 @@
 "# cli-number-guessing" 
+"# cli-number-guessing" 
---
+++ index.js
@@ -0,0 +1,16 @@
+#! /usr/bin/env node
+import inquirer from "inquirer";
+const randomNumber = Math.floor(Math.random() * 10 + 1);
+const answers = await inquirer.prompt([
+    {
+        name: "userGuessNumber",
+        type: "number",
+        message: "please guess a number between 1-10"
+    },
+]);
+if (answers.userGuessNumber === randomNumber) {
+    console.log("congratulation you guess right number");
+}
+else {
+    console.log("you guess wrong number");
+}
